a5aab6046b00ef3805da83b2d624a75f5fb96de6
drahmetacik <a.acik@icloud.com>
2025-04-07 11:13:26 +0100

    refactor ProductController unhappy path tests to use constants for endpoint URLs

--- src/test/java/org/ahmet/fakeapistore/ProductControllerUnhappyPaths.java
+++ src/test/java/org/ahmet/fakeapistore/ProductControllerUnhappyPaths.java
@@ -1,64 +1,70 @@
 package org.ahmet.fakeapistore;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+    import io.restassured.RestAssured;
+    import io.restassured.http.ContentType;
+    import org.junit.jupiter.api.BeforeAll;
+    import org.junit.jupiter.api.Disabled;
+    import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+    import static io.restassured.RestAssured.given;
+    import static org.hamcrest.Matchers.equalTo;
 
-public class ProductControllerUnhappyPaths {
+    public class ProductControllerUnhappyPaths {
 
-    @BeforeAll
-    public static void setup() {
-        RestAssured.baseURI = "https://fakestoreapi.com";
+        private static final String BASE_URI = "https://fakestoreapi.com";
+        private static final String PRODUCTS_ENDPOINT = "/products";
+
+        @BeforeAll
+        public static void setup() {
+            RestAssured.baseURI = BASE_URI;
+        }
+
+        @Test
+        @Disabled("This test is ignored because it is failing")
+        public void shouldReturnErrorForInvalidProductIdInGetProduct() {
+            given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get(PRODUCTS_ENDPOINT + "/invalid-id")
+                .then()
+                .statusCode(400)
+                .body("message", equalTo("The JSON input text should neither be null nor empty."));
+        }
+
+        @Test
+        @Disabled("This test is ignored because it is failing")
+        public void shouldReturnErrorForUndefinedBodyInAddProduct() {
+            given()
+                .contentType(ContentType.JSON)
+                .body("{}")
+                .when()
+                .post(PRODUCTS_ENDPOINT)
+                .then()
+                .statusCode(400)
+                .body("message", equalTo("data is undefined"));
+        }
+
+        @Test
+        public void shouldReturnErrorForUndefinedBodyOrNullIdInEditProduct() {
+            given()
+                .contentType(ContentType.JSON)
+                .body("{}")
+                .pathParam("id", "null")
+                .when()
+                .put(PRODUCTS_ENDPOINT + "/{id}")
+                .then()
+                .statusCode(400)
+                .body("message", equalTo("something went wrong! check your sent data"));
+        }
+
+        @Test
+        public void shouldReturnErrorForNullIdInDeleteProduct() {
+            given()
+                .pathParam("id", "null")
+                .when()
+                .delete(PRODUCTS_ENDPOINT + "/{id}")
+                .then()
+                .statusCode(400)
+                .body("message", equalTo("product id should be provided"));
+        }
     }
-
-//    @Test
-//    public void shouldReturnErrorForInvalidProductIdInGetProduct() {
-//        given()
-//            .pathParam("id", "invalid-id")
-//        .when()
-//            .get("/products/{id}")
-//        .then()
-//            .statusCode(400)
-//            .body("message", equalTo("Invalid product ID"));
-//    }
-//
-//    @Test
-//    public void shouldReturnErrorForUndefinedBodyInAddProduct() {
-//        given()
-//            .contentType(ContentType.JSON)
-//            .body("{}") // Provide an empty JSON object instead of null
-//        .when()
-//            .post("/products")
-//        .then()
-//            .statusCode(400)
-//            .body("message", equalTo("data is undefined"));
-//    }
-
-    @Test
-    public void shouldReturnErrorForUndefinedBodyOrNullIdInEditProduct() {
-        given()
-            .contentType(ContentType.JSON)
-            .body("{}")
-            .pathParam("id", "null")
-        .when()
-            .put("/products/{id}")
-        .then()
-            .statusCode(400)
-            .body("message", equalTo("something went wrong! check your sent data"));
-    }
-
-    @Test
-    public void shouldReturnErrorForNullIdInDeleteProduct() {
-        given()
-            .pathParam("id", "null")
-        .when()
-            .delete("/products/{id}")
-        .then()
-            .statusCode(400)
-            .body("message", equalTo("product id should be provided"));
-    }
-}
