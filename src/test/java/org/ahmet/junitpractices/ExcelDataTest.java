ecb99c19a25427e04ca11769b0ef4f8f15958171
drahmetacik <a.acik@icloud.com>
2025-04-14 23:10:29 +0100

    add testExcelData_Meaningful_Student_List method to ExcelDataTest for processing meaningful student data from Excel

--- src/test/java/org/ahmet/junitpractices/ExcelDataTest.java
+++ src/test/java/org/ahmet/junitpractices/ExcelDataTest.java
@@ -15,4 +15,13 @@ public class ExcelDataTest {
             System.out.println("Row: " + row);
         }
     }
+
+    @Test
+    public void testExcelData_Meaningful_Student_List() {
+        ExcelUtil excelUtil = new ExcelUtil("src/test/resources/Meaningful_Student_List.xlsx", "MeaningfulStudentList");
+
+        for (Map<String, String> row : excelUtil.getDataList()) {
+            System.out.println("Row: " + row);
+        }
+    }
 }
